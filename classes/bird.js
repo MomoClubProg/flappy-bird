18c6a987908ce74cbc497e748bf1f97a1cfa0500
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-22 13:18:04 -0400

    changed gravity constant

--- classes/bird.js
+++ classes/bird.js
@@ -4,19 +4,17 @@ class Bird {
         this.ay = 0;
         this.x = x;
         this.y = y;
-
     }
     render() {
         ellipse(this.x, this.y, 10, 10);
     }
     update() {
         this.vy -= this.ay
-        this.vy = this.vy + 0.05;
+        this.vy = this.vy + 0.1;
         this.y = this.y + this.vy;
         this.ay *= 0;
     }
     jump() {
         this.ay = 3;
-
     }
 }

81380c6e0bc049b5e596089575553d1e186b9026
matiasvlevi <matiasvlevi@gmail.com>
2022-04-13 00:03:17 -0400

    clear main.js
    - Menu & Score related methods/values in the Game class
    
    - Keypressed in ./controls/keyboard.js
    
    - refactor

--- public/main.js
+++ public/main.js
@@ -1,66 +1,65 @@
 let bird;
+let game;
 let world;
 let pipes = [];
+
 let wnx = 1920;
 let wny = 1080;
+let pipeNumber = 6;
 
-
-function restartMenu(){
-
-    stroke(255)
-    fill(255)
-    /*textSize(72)
-    textAlign(CENTER);0
-    text("YOU LOST",300,300);*/
-
-    textSize(50);
-    textAlign(CENTER);
-    text("Press space to start",300, 350);
-
-}
-
-
-
-function initGame() {
-
-    noLoop();
-    restartMenu();
-    pipes = [];
-    pipes.push(new Pipe(1*wny/4 + wny , random(250, wny-250)));
-    pipes.push(new Pipe(2*wny/4 + wny, random(250, wny-250)));
-    bird = new Bird(wnx/2, wny/2);
-    world = new World(4);
-    createCanvas(wnx, wny);
-}
 
 function setup() {
-    initGame();
-    loop();
-    createCanvas(wnx, wny);
+  // Create game instance
+  game = new Game();
+
+  initGame();
+  loop();
 }
+
+function initGame() {
+  noLoop();
+
+  // Display menu screen
+  game.renderMenu();
+  game.newAttempt();
+
+
+  // Create World instance
+  world = new World(4);
+
+  // Initiate pipes;
+  pipes = [];
+  for (let i = 0; i < pipeNumber; i++) {
+    pipes.push(new Pipe(
+      i * wnx / pipeNumber + ((pipeNumber - 1) * wnx) / pipeNumber,
+      random(250, wny - 250)
+    ));
+  }
+
+  // Create player
+  bird = new Bird(wnx / 2, wny / 2);
+
+  createCanvas(wnx, wny);
+}
+
 
 function draw() {
-    // Background 
-    background(80,200,255);
+  // Update every actor
+  world.update();
+  pipes.forEach(pipe => {
+    pipe.update();
+    pipe.checkScore(game);
+  });
+  bird.update();
 
-    
-    // Update every actor
-    world.update();
-    pipes.forEach(pipe => pipe.update());
-    bird.update();
+  // Check if game ended
+  if (bird.isOutOfBounds()) initGame();
+  if (bird.isColliding(pipes)) initGame();
 
-    // Check if game ended
-    if(bird.isOutOfBounds()) initGame();
-    if(bird.isColliding(pipes)) initGame();
+  // Render every actor
+  world.render();
+  pipes.forEach(pipe => pipe.render());
+  bird.render();
 
-
-    // Render every actor
-    world.render();
-    pipes.forEach(pipe => pipe.render());
-    bird.render();
-}
-
-function keyPressed() {
-    if (keyCode === 32) bird.jump();
-    if (keyCode === 32) (isLooping() ? (()=>{})(): loop());
+  game.renderCounters();
 }

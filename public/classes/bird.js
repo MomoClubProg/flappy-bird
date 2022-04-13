e7aadd5e1181aac335935a35f8f1998b4955432e
matiasvlevi <matiasvlevi@gmail.com>
2022-04-13 00:04:50 -0400

    minor refactor

--- public/classes/bird.js
+++ public/classes/bird.js
@@ -1,30 +1,32 @@
 class Bird {
-    constructor(x, y) {
-        this.vy = 0;
-        this.ay = 0;
-        this.x = x;
-        this.y = y;
-        //this.bird = loadImage('./assets/bird.png');
-    }
-    isColliding = (pipes) => pipes.map(pipe => 
-        pipe.isColliding(this)
-    ).includes(true);
-    
-    render() {
-        ellipse(this.x, this.y, 10, 10);
-    }
-    
-    isOutOfBounds() {
-        return this.y<=0 || this.y>=wny;
-    }
+  constructor(x, y) {
+    this.vy = 0;
+    this.ay = 0;
+    this.x = x;
+    this.y = y;
+    //this.bird = loadImage('./assets/bird.png');
+  }
+  isColliding = (pipes) => pipes.map(pipe =>
+    pipe.isColliding(this)
+  ).includes(true);
 
-    update() {
-        this.vy -= this.ay
-        this.vy = this.vy + 0.1;
-        this.y = this.y + this.vy;
-        this.ay *= 0;
-    }
-    jump() {
-        this.ay = 3;
-    }
+  render() {
+    ellipse(this.x, this.y, 18, 18);
+  }
+
+  isOutOfBounds() {
+    return this.y <= 0 || this.y >= wny;
+  }
+
+  update() {
+    this.ay -= 0.3;
+    this.vy -= this.ay
+    this.y = this.y + this.vy;
+    this.vy *= 0.97;
+    this.ay *= 0;
+  }
+  jump() {
+    this.ay = 9
+
+  }
 }

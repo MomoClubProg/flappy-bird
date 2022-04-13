05a6b45403ec14dad4b01317ca8f49e1cdaadb3b
matiasvlevi <matiasvlevi@gmail.com>
2022-04-13 00:03:50 -0400

    keyboard.js

---
+++ public/controls/keyboard.js
@@ -0,0 +1,7 @@
+function keyPressed() {
+  // Bird jump mechanic
+  if (keyCode === 32) bird.jump();
+
+  // If space is pressed while the game is stopped, restart the game
+  if (keyCode === 32)(isLooping() ? (() => {})() : loop());
+}

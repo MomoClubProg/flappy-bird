ca52830582d1bc2554e11ec96c0788fa535647ab
matiasvlevi <matiasvlevi@gmail.com>
2022-04-13 00:05:31 -0400

    refactor world

--- public/classes/world.js
+++ public/classes/world.js
@@ -1,53 +1,55 @@
 class World {
-    constructor(n) {
-        this.imageLayers = [];
-        this.xoffset = [];
-        for (let i = 0; i < n; i++) {
-            this.imageLayers.push(
-                loadImage(`./assets/background/layer${i}.png`)
-            );
-            this.xoffset.push((i+1)*1);
-        }
-    }
-    update() {
+  constructor(n) {
 
-        this.xoffset = this.xoffset.map((off, i)=>{
-            off-=i;
-            if (off <= -wnx*2) {
-                off = wnx*2;
-            }
-            return off;
-        });
-        console.log(this.xoffset); 
+    this.imageLayers = [];
+    this.xoffset = [];
+    for (let i = 0; i < n; i++) {
+      this.imageLayers.push(
+        loadImage(`./assets/background/layer${i}.png`)
+      );
+      this.xoffset.push((i + 1) * 1);
     }
-    render() {
-        push();
-        //scale(1);
-        let i=0;
-        for (let img of this.imageLayers) {
-            image(
-                img,
-                this.xoffset[i],
-                0,
-                3840,
-                1080
-            );
-            i++;
-        }
-        i=0;
-        for (let img of this.imageLayers) {
-            image(
-                img,
-                this.xoffset[i] + 2*(wnx),
-                0,
-                3840,
-                1080
-            );
-            i++;
-        }
-        pop();
-        fill(0,0,0,80);
-        rect(0,0,wnx,wny);
-        fill(255);
+
+  }
+
+  update() {
+    // Offset each layer
+    this.xoffset = this.xoffset.map((off, i) => {
+      // Increment offset (first element being slower than the last)
+      off -= (i + 1) * 0.5 * SPEED;
+
+      // Restart position if layer is out of range
+      if (off <= -wnx * 2) off += wnx * 2;
+
+      return off;
+    });
+  }
+
+
+  render() {
+    background(80, 200, 255);
+    for (let i = 0; i < this.imageLayers.length; i++) {
+      // Image 1
+      image(
+        this.imageLayers[i],
+        this.xoffset[i],
+        i * -10,
+        3840,
+        1080
+      );
+      // Image 2
+      image(
+        this.imageLayers[i],
+        this.xoffset[i] + 2 * (wnx),
+        i * -10,
+        3840,
+        1080
+      );
     }
+
+    push();
+    fill(0, 0, 0, 80);
+    rect(0, 0, wnx, wny);
+    pop();
+  }
 }
